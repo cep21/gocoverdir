d289ad2e30deb656bbc102c27589b3173ce9227b
Jack Lindamood <jack@signalfx.com>
2015-07-22 11:01:20 -0700

    Fix testing dependency

--- gocoverdir_test.go
+++ gocoverdir_test.go
@@ -4,15 +4,19 @@ import (
 	"flag"
 	"fmt"
 	"testing"
-
-	"github.com/stretchr/testify/assert"
 )
+
+func noError(t *testing.T, err error) {
+	if err != nil {
+		panic(fmt.Sprintf("Saw error %s", err))
+	}
+}
 
 func TestGocoverdir(t *testing.T) {
 	m := gocoverdir{}
 	fs := flag.NewFlagSet("testsetup", flag.PanicOnError)
 	m.setupFlags(fs)
-	assert.NoError(t, fs.Parse([]string{""}))
+	noError(t, fs.Parse([]string{""}))
 	fmt.Printf("%+v", m)
-	assert.NoError(t, m.setup())
+	noError(t, m.setup())
 }
